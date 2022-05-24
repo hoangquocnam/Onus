f08254dc87a4d901cfedd39e3fcd642545502775
hqnam <hoangquocnam021117@gmail.com>
2022-05-24 21:24:43 +0700

    main - create test place

---
+++ client/src/test/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function TestComponents(){
+    return(
+        <div>
+            <h1>Test Components</h1>
+        </div>
+    )
+}
+
+export default TestComponents;
