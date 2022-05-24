11bb134bd39882b6191a5326b2f38d755501b312
hqnam <hoangquocnam021117@gmail.com>
2022-05-24 22:11:00 +0700

    tutorial for members

--- client/src/test/index.js
+++ client/src/test/index.js
@@ -2,9 +2,12 @@ import React from "react";
 
 
 function TestComponents(){
+    // TODO: Add your code here
+
     return(
         <div>
             <h1>Test Components</h1>
+            <h2>Nam dep trai</h2>
         </div>
     )
 }
