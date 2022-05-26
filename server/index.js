7470ef6cf8b36e3c2189b6df07333b87e0c874ae
hqnam <hoangquocnam021117@gmail.com>
2022-05-26 08:18:31 +0700

    [OS-5] connect db first, then run server

--- server/index.js
+++ server/index.js
@@ -1,30 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-
-const port = 8000;
-mongoose.connect(process.env.DATABASE);
-
-const connection = mongoose.connection
-
-connection
-    .on('connected', () => {
-        console.log('Connected Database')
-    })
-    .on('disconnected', () => {
-        console.log('Disconnect Database');
-    })
-    .on('error', (error) => {
-        console.log(error)
-    })
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const server = express();
 
-server.use(cors())
+server.use(cors());
 
+const port = 8000;
+mongoose.connect(process.env.DATABASE);
+const connection = mongoose.connection;
 
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-})
+connection
+  .on("connected", () => {
+    console.log("Connected Database");
+    server.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .on("disconnected", () => {
+    console.log("Disconnect Database");
+  })
+  .on("error", (error) => {
+    console.log(error);
+  });
