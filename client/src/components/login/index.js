f3fc7d46dccc30dd8d22d9923f5ebd708b013d8f
Tâm Lê <leduytam600@gmail.com>
2022-05-31 21:08:11 +0700

    [OS-9] hotfit ui login page

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import "../../styles/login.css"
+import React from "react";
+import "../../styles/login.css";
 
 function LoginForm() {
-    return (
-        <div className="login__page">
-            <div className="login__container">
-                <h2 className="login__title">Log in</h2>
-                
-                <div className="login__body">
-                    <form className="login__form">
-                        <input type="email" className="inputLogin__email" placeholder="Enter email" />
-                        <input type="password" className="inputLogin__password" placeholder="Enter password" />
-                        <input className="login__buttonSubmit" type="button" value="Log in" />
+  return (
+    <div className="login__page">
+      <div className="login__container">
+        <h2 className="login__title">Log in</h2>
 
-                        <p>OR</p>
-                    </form>
-                </div>
-                    
-                <div className='login__footer'>
-                    <a className="link__signUp" href="#">Create a new account?</a>
-                </div>
-            </div>
+        <div className="login__body">
+          <form className="login__form">
+            <input
+              type="email"
+              className="inputLogin__email"
+              placeholder="Enter email"
+            />
+            <input
+              type="password"
+              className="inputLogin__password"
+              placeholder="Enter password"
+            />
+            <input
+              className="login__buttonSubmit"
+              type="button"
+              value="Log in"
+            />
+          </form>
+          <p>OR</p>
         </div>
-    )
+
+        <div className="login__footer">
+          <a className="link__signUp" href="#">
+            Create a new account?
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
