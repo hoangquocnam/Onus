74f34b15107b2328d7286a89c15c19b224d98dc7
Nhat Minh <nhatminhc14@gmail.com>
2022-06-04 01:34:16 +0700

    [OS-7] Validate Sign Up Form

--- client/src/components/signup/index.jsx
+++ client/src/components/signup/index.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import Button from "./button";
+import React, { useState } from "react";
+import validation from "./signUpValidation";
 function SignUp() {
-	const submit = (e) => {
+	const [values, setValues] = useState({
+		fullName: "",
+		userName: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+		phoneNumber: "",
+	});
+	const [errors, setErrors] = useState({});
+	const handleFormSubmit = (e) => {
 		e.preventDefault();
-		console.log("submit");
-	}
+		setErrors(validation(values));
+	};
+	const handleChange = (e) => {
+		setValues({
+			...values,
+			[e.target.name]: e.target.value,
+		});
+	};
 	return (
 		<div className="signup">
 			<div className="signup__wrapper">
@@ -14,40 +29,69 @@ function SignUp() {
 						className="signup__form--name"
 						placeholder="Full name"
 						autoComplete="none"
+						name="fullName"
+						value={values.fullName}
+						onChange={handleChange}
 					></input>
+					{errors.fullName && <p className="error">{errors.fullName}</p>}
 					<input
 						className="signup__form-username"
 						placeholder="Username"
+						autoComplete="none"
+						name="userName"
+						value={values.userName}
+						onChange={handleChange}
 					></input>
+					{errors.userName && <p className="error">{errors.userName}</p>}
 					<input
-						type="email"
+						type="text"
 						className="signup__form-email"
 						placeholder="Email"
 						autoComplete="none"
-						required
+						name="email"
+						// required
+						value={values.email}
+						onChange={handleChange}
 					></input>
+					{errors.email && <p className="error">{errors.email}</p>}
 					<input
 						type="password"
 						className="signup__form-password"
 						placeholder="Password"
 						autoComplete="none"
-						required
+						name="password"
+						// required
+						value={values.password}
+						onChange={handleChange}
 					></input>
+					{errors.password && <p className="error">{errors.password}</p>}
 					<input
 						type="password"
 						className="signup__form-confirm-password"
 						placeholder="Confirm password"
-						required
+						// required
+						name="confirmPassword"
 						autoComplete="none"
+						value={values.confirmPassword}
+						onChange={handleChange}
 					></input>
+					{errors.confirmPassword && (
+						<p className="error">{errors.confirmPassword}</p>
+					)}
 					<input
 						type="tel"
 						className="signup__form-phone-number"
 						placeholder="Phone number"
-						required
+						// required
+						name="phoneNumber"
 						autoComplete="none"
+						value={values.phoneNumber}
+						onChange={handleChange}
 					></input>
-					<Button buttonText={"Create New Account"} submit={submit} />
+					{errors.phoneNumber && <p className="error">{errors.phoneNumber}</p>}
+					<button className="signup__btn" onClick={handleFormSubmit}>
+						Create New Account
+					</button>
 				</form>
 			</div>
 		</div>
