94502c3f487ff20de1ea7693381a7907256f2e74
Tâm Lê <leduytam600@gmail.com>
2022-06-04 19:24:52 +0700

    [OS-6] convert native js code to reactjs code

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,96 +1,49 @@
-import React from "react";
+import { createRef, useState } from "react";
 import "../../styles/login.css";
+import validate from "./validate";
 
-function LoginForm() {
-  const specialChars = "!@#$%^&*()_+{}|:\"<>?[]\\;',./";
-  const emailRegex =
-    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-  const passwordValidLength = { min: 8, max: 14 };
+function LogInForm() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    errors: {
+      email: "",
+      password: "",
+    },
+  });
 
-  function submit(e) {
+  const emailInputRef = createRef();
+  const passwordInputRef = createRef();
+
+  function handleInputChange(e) {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+      errors: {
+        email: "",
+        password: "",
+      },
+    });
+  }
+
+  function handleFormSubmit(e) {
     e.preventDefault();
+    const errors = validate(values);
+    setValues({ ...values, errors });
 
-    const inputEmail = document.getElementsByClassName("inputLogin__email")[0];
-    const inputPassword = document.getElementsByClassName(
-      "inputLogin__password"
-    )[0];
-
-    if (!validateEmail(inputEmail.value)) {
-      showError(inputEmail, "Email is not valid");
-      return;
+    if (errors.email) {
+      emailInputRef.current.focus();
+      emailInputRef.current.select();
     }
 
-    if (!validatePassword(inputPassword.value)) {
-      showError(inputPassword, "Password is not valid");
-      return;
+    if (errors.password) {
+      passwordInputRef.current.focus();
+      passwordInputRef.current.select();
     }
-
-    hideError(inputEmail);
-    hideError(inputPassword);
-  }
-
-  function showError(input, errorMessage) {
-    const error = document.getElementsByClassName("login__errorMessage")[0];
-    input.focus();
-    input.select();
-    input.classList.add("error");
-    error.textContent = errorMessage;
-    error.classList.remove("login__errorMessage--hidden");
-  }
-
-  function hideError(input) {
-    const error = document.getElementsByClassName("login__errorMessage")[0];
-    error.classList.add("login__errorMessage--hidden");
-    input.classList.remove("error");
-  }
-
-  function validateEmail(email) {
-    return emailRegex.test(email);
-  }
-
-  function validatePassword(password) {
-    if (password.length < passwordValidLength.min) {
-      return false;
-    }
-
-    if (password.length > passwordValidLength.max) {
-      return false;
-    }
-
-    let isContainNumber = false;
-    let isContainUpperCase = false;
-    let isContainLowerCase = false;
-    let isContainSpecialCharacter = false;
-
-    for (const ch of password) {
-      console.log(ch);
-      if (ch >= "0" && ch <= "9") {
-        isContainNumber = true;
-      }
-
-      if (ch >= "A" && ch <= "Z") {
-        isContainUpperCase = true;
-      }
-
-      if (ch >= "a" && ch <= "z") {
-        isContainLowerCase = true;
-      }
-
-      if (specialChars.indexOf(ch) !== -1) {
-        isContainSpecialCharacter = true;
-      }
-    }
-    
-    return (
-      isContainNumber &&
-      isContainUpperCase &&
-      isContainLowerCase &&
-      isContainSpecialCharacter
-    );
   }
 
   return (
-    <div className="login__page">
+    <div className="login-page">
       <div className="login__container">
         <h2 className="login__title">Log in</h2>
 
@@ -98,37 +51,49 @@ function LoginForm() {
           <form className="login__form">
             <input
               type="email"
-              className="inputLogin__email"
+              className={`login__email-input${
+                values.errors.email ? " login__input--error" : ""
+              }`}
               placeholder="Enter email"
-              onChange={(e) => hideError(e.target)}
-              autoFocus
+              onChange={handleInputChange}
+              name="email"
+              value={values.email}
+              ref={emailInputRef}
             />
 
             <input
               type="password"
-              className="inputLogin__password"
+              className={`login__password-input${
+                values.errors.password ? " login__input--error" : ""
+              }`}
               placeholder="Enter password"
-              minLength={passwordValidLength.min}
-              maxLength={passwordValidLength.max}
-              onChange={(e) => hideError(e.target)}
+              minLength={8}
+              maxLength={14}
+              onChange={handleInputChange}
+              name="password"
+              value={values.password}
+              ref={passwordInputRef}
             />
 
-            <p className="login__errorMessage login__errorMessage--hidden">
-              Error message
-            </p>
+            {(values.errors.email || values.errors.password) && (
+              <div className="login__error-message">
+                {values.errors.email || values.errors.password}
+              </div>
+            )}
 
             <input
-              className="login__buttonSubmit"
-              type="submit"
+              type="button"
+              className="login__submit-btn"
               value="Log in"
-              onClick={submit}
+              onClick={handleFormSubmit}
             />
           </form>
+
           <p>OR</p>
         </div>
 
         <div className="login__footer">
-          <a className="link__signUp" href="#">
+          <a href="#" className="login__signup-link">
             Create a new account?
           </a>
         </div>
@@ -137,4 +102,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm;
+export default LogInForm;
