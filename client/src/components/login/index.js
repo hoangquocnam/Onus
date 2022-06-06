e0da08cb2e0f0825334638b202595eae808eb98b
Tâm Lê <leduytam600@gmail.com>
2022-06-06 11:10:38 +0700

    [OS-6] validate email, password login page (#6)
    * [OS-6] validate email, password
    
    * [OS-6] convert native js code to reactjs code

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,9 +1,49 @@
-import React from "react";
+import { createRef, useState } from "react";
 import "../../styles/login.css";
+import validate from "./validate";
 
-function LoginForm() {
+function LogInForm() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    errors: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const emailInputRef = createRef();
+  const passwordInputRef = createRef();
+
+  function handleInputChange(e) {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+      errors: {
+        email: "",
+        password: "",
+      },
+    });
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const errors = validate(values);
+    setValues({ ...values, errors });
+
+    if (errors.email) {
+      emailInputRef.current.focus();
+      emailInputRef.current.select();
+    }
+
+    if (errors.password) {
+      passwordInputRef.current.focus();
+      passwordInputRef.current.select();
+    }
+  }
+
   return (
-    <div className="login__page">
+    <div className="login-page">
       <div className="login__container">
         <h2 className="login__title">Log in</h2>
 
@@ -11,25 +51,49 @@ function LoginForm() {
           <form className="login__form">
             <input
               type="email"
-              className="inputLogin__email"
+              className={`login__email-input${
+                values.errors.email ? " login__input--error" : ""
+              }`}
               placeholder="Enter email"
+              onChange={handleInputChange}
+              name="email"
+              value={values.email}
+              ref={emailInputRef}
             />
+
             <input
               type="password"
-              className="inputLogin__password"
+              className={`login__password-input${
+                values.errors.password ? " login__input--error" : ""
+              }`}
               placeholder="Enter password"
+              minLength={8}
+              maxLength={14}
+              onChange={handleInputChange}
+              name="password"
+              value={values.password}
+              ref={passwordInputRef}
             />
+
+            {(values.errors.email || values.errors.password) && (
+              <div className="login__error-message">
+                {values.errors.email || values.errors.password}
+              </div>
+            )}
+
             <input
-              className="login__buttonSubmit"
               type="button"
+              className="login__submit-btn"
               value="Log in"
+              onClick={handleFormSubmit}
             />
           </form>
+
           <p>OR</p>
         </div>
 
         <div className="login__footer">
-          <a className="link__signUp" href="#">
+          <a href="#" className="login__signup-link">
             Create a new account?
           </a>
         </div>
@@ -38,4 +102,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm;
+export default LogInForm;
