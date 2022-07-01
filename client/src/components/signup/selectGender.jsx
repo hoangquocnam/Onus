a9e535a09db1658a672466bcf1ef5d707642af29
Tâm Lê <leduytam600@gmail.com>
2022-07-02 00:40:05 +0700

    Restyling Sign Up Page (#11)
    * [OS-15] Restyling Sign Up UI
    
    Gender not yet validate
    
    * [OS-15] Restyling Sign Up Page
    
    Co-authored-by: Nhat Minh <nhatminhc14@gmail.com>

---
+++ client/src/components/signup/selectGender.jsx
@@ -0,0 +1,64 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+
+function SelectGender({
+  options: optionsProp,
+  className: classNameProp,
+  onChange: onChangeProp,
+}) {
+  const [option, setOption] = useState(
+    optionsProp.length > 0 ? optionsProp[0] : null
+  );
+
+  useEffect(() => {
+    if (onChangeProp) {
+      onChangeProp({
+        ...option,
+        index: 0,
+      });
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function handleChange(e) {
+    const selected = {
+      value: e.target.value,
+      label: e.target.options[e.target.selectedIndex].text,
+      index: e.target.selectedIndex,
+    };
+
+    setOption(selected);
+
+    if (onChangeProp) {
+      onChangeProp(selected);
+    }
+  }
+
+  return (
+    <select className={classNameProp} onChange={handleChange}>
+      {optionsProp.map((item) => (
+        <option
+          key={item.value}
+          value={item.value}
+          defaultValue={item.value === option.value}
+        >
+          {item.label}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+SelectGender.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+export default SelectGender;
--- client/src/utils/validate.js
+++ client/src/utils/validate.js
@@ -1,40 +1,5 @@
 import { EMAIL_REGEX } from "../constants";
 
-export const validationSignUp = (values) => {
-  let errors = {};
-  const emailRegex = /^\w+([\\.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-  const phoneRegex = /^\d{10}$/;
-  if (!values.fullName) {
-    errors.fullName = "Full name is required";
-  }
-  if (!values.userName) {
-    errors.userName = "Username is required";
-  }
-  if (!values.email) {
-    errors.email = "Email is required";
-  } else if (!emailRegex.test(values.email)) {
-    errors.email = "Email is invalid";
-  }
-
-  if (!values.password) {
-    errors.password = "Password is required";
-  }
-  if (!values.confirmPassword) {
-    errors.confirmPassword = "Confirm password is required";
-  } else if (values.password.length < 6) {
-    errors.password = "Password must be at least 6 characters";
-  }
-  if (values.password !== values.confirmPassword) {
-    errors.confirmPassword = "Passwords must match";
-  }
-  if (!values.phoneNumber) {
-    errors.phoneNumber = "Phone number is required";
-  } else if (!phoneRegex.test(values.phoneNumber)) {
-    errors.phoneNumber = "Phone number is invalid";
-  }
-  return errors;
-};
-
 function validateEmail(email) {
   if (email.trim() === "") {
     return "Email is required";
