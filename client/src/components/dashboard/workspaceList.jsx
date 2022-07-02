aabbced2c684e5c8ed0a01d4a07b7878a4880a33
hqnam <hoangquocnam021117@gmail.com>
2022-07-02 15:00:21 +0700

    [0S-24] Complete the UI design for workspace list view

--- client/src/components/dashboard/workspaceList.jsx
+++ client/src/components/dashboard/workspaceList.jsx
@@ -2,15 +2,50 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+import { BsArrowRight } from "react-icons/bs";
+import _ from "lodash";
 import "../../styles/components/workspaceListView.css";
 
 function WorkspaceItemCard(props) {
-  const { title, description, itemId } = props;
+  const { title, description, members } = props;
+
+  const randomColor = ["#EEF7FB", "#f4f4f4", "#F8F1FF", "#FEF7EF"];
 
   return (
-    <div className="workspace-item-card" tabIndex={0}>
+    <div className="workspace-item-card" tabIndex={0} style = {{
+      backgroundColor: randomColor[Math.floor(Math.random() * randomColor.length)]
+    }}>
       <p className="workspace-item-card-title">{title}</p>
       <p className="workspace-item-card-description">{description}</p>
+
+      <div className="workspace-item-card-footer">
+        <div className="workspace-item-card-members">
+          {_.isArray(members)
+            ? members.slice(0, 6).map((member, index) => {
+                return (
+                  <div
+                    className="workspace-item-card-member"
+                    style={{ left: 20 * index + "px" }}
+                  >
+                    {index <= 4 ? (
+                      <img
+                        src={member}
+                        alt="avatar"
+                        style={{
+                          width: "30px",
+                          height: "30px",
+                        }}
+                      />
+                    ) : (
+                      <div>{`+${members.length - index}`}</div>
+                    )}
+                  </div>
+                );
+              })
+            : "members"}
+        </div>
+        <BsArrowRight size={20} className="workspace-item-card-arrow" />
+      </div>
     </div>
   );
 }
@@ -23,7 +58,6 @@ function LeftArrow() {
     <button
       disabled={isFirstItemVisible}
       className="workspace-list-view-arrow"
-      style={{ width: "50px", height: "50px" }}
       onClick={() => scrollPrev()}
     >
       <AiOutlineArrowLeft size={20} />
@@ -59,13 +93,14 @@ export function WorkspaceListView() {
         <p className="workspaceList__title">Workspaces</p>
 
         <div className="workspaceList__container">
-          <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} scrollContainerClassName>
+          <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
             {workspaces.map((workspace) => (
               <WorkspaceItemCard
                 key={workspace.id}
                 title={workspace.title}
                 description={workspace.description}
                 itemId={workspace.id}
+                members={workspace.members}
               />
             ))}
           </ScrollMenu>
