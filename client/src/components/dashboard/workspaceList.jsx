5e6a21d434fe47841177f4bef96995ebd339715c
hqnam <hoangquocnam021117@gmail.com>
2022-07-02 11:28:48 +0700

    [OS-24] Handle the scroll bar

--- client/src/components/dashboard/workspaceList.jsx
+++ client/src/components/dashboard/workspaceList.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 import "../../styles/components/workspaceListView.css";
 
 function WorkspaceItemCard(props) {
   const { title, description, itemId } = props;
 
-  return <div className="workspace-item-card" tabIndex={0}></div>;
+  return (
+    <div className="workspace-item-card" tabIndex={0}>
+      <p className="workspace-item-card-title">{title}</p>
+      <p className="workspace-item-card-description">{description}</p>
+    </div>
+  );
 }
 
 function LeftArrow() {
@@ -15,10 +21,13 @@ function LeftArrow() {
 
   return (
     <button
-      type="button"
+      disabled={isFirstItemVisible}
+      className="workspace-list-view-arrow"
       style={{ width: "50px", height: "50px" }}
       onClick={() => scrollPrev()}
-    />
+    >
+      <AiOutlineArrowLeft size={20} />
+    </button>
   );
 }
 
@@ -26,11 +35,13 @@ function RightArrow() {
   const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
 
   return (
-    <input
-      type="button"
-      style={{ width: "50px", height: "50px" }}
+    <button
+      disabled={isLastItemVisible}
+      className="workspace-list-view-arrow"
       onClick={() => scrollNext()}
-    />
+    >
+      <AiOutlineArrowRight size={20} />
+    </button>
   );
 }
 
@@ -46,19 +57,19 @@ export function WorkspaceListView() {
     <React.StrictMode>
       <div className="workspaceListView">
         <p className="workspaceList__title">Workspaces</p>
-      </div>
 
-      <div className="workspaceList__container">
-        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-          {workspaces.map((workspace) => (
-            <WorkspaceItemCard
-              key={workspace.id}
-              title={workspace.title}
-              description={workspace.description}
-              itemId={workspace.id}
-            />
-          ))}
-        </ScrollMenu>
+        <div className="workspaceList__container">
+          <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} scrollContainerClassName>
+            {workspaces.map((workspace) => (
+              <WorkspaceItemCard
+                key={workspace.id}
+                title={workspace.title}
+                description={workspace.description}
+                itemId={workspace.id}
+              />
+            ))}
+          </ScrollMenu>
+        </div>
       </div>
     </React.StrictMode>
   );
