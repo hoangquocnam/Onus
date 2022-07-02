8eee7809e3c71d6a2fab331bcf5acad4c85514f3
Tấn Sang <btsang20@clc.fitus.edu.vn>
2022-07-02 14:12:23 +0700

    [OS-27] Create User Profile

---
+++ client/src/components/userprofile/index.jsx
@@ -0,0 +1,15 @@
+import { createRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { methods, URL_Requests } from "../../APIs";
+import routes from "../../routes";
+import { AccountConsumer } from "../../stores/account";
+import "../../styles/pages/userprofile.css";
+
+function UserProfile()
+{
+    return(
+        <div></div>
+    );
+}
+
+export default UserProfile;
