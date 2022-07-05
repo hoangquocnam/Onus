a447ab56b7d91e00d7c825a3b94e912199d2d9d6
Tâm Lê <leduytam600@gmail.com>
2022-07-05 23:46:03 +0700

    [OS-29] update workspace header, card

--- client/src/components/workspace/workspaceHeader.jsx
+++ client/src/components/workspace/workspaceHeader.jsx
@@ -1,48 +1,60 @@
-import { FaGlobeAsia, FaRegHeart } from "react-icons/fa";
-import { GrStorage } from "react-icons/gr";
-import { MdOutlineSettings } from "react-icons/md";
+import {
+  FaBars,
+  FaFilter,
+  FaGlobeAsia,
+  FaRegStar,
+  FaUserPlus,
+} from "react-icons/fa";
 import "../../styles/components/workspaceHeader.css";
 
 function WorkspaceHeader({ title }) {
   return (
     <div className="workspace-header">
-      <div className="workspace-header__wrapper">
-        <div className="workspace-header__wrapper--left">
-          <h1 className="workspace-header__title">{title}</h1>
+      <div className="workspace-header__left">
+        <h1 className="workspace-header__title">{title}</h1>
 
-          <div className="workspace-header__utility">
-            <FaRegHeart size={25} className="workspace-header__bookmark" />
+        <div className="workspace-header__utility">
+          <FaRegStar size={25} className="workspace-header__bookmark" />
 
-            <div className="workspace-header__separator"></div>
+          <div className="workspace-header__separator"></div>
 
-            <div className="workspace-header__visibility">
-              <FaGlobeAsia size={25} />
-              <p>Public</p>
-            </div>
-
-            <div className="workspace-header__separator"></div>
-
-            <GrStorage size={25} className="workspace-header__archive" />
+          <div className="workspace-header__visibility">
+            <FaGlobeAsia size={25} />
+            <p>Public</p>
           </div>
+
+          <div className="workspace-header__separator"></div>
         </div>
 
-        <div className="workspace-members">
+        <div className="workspace-header__members">
           {Array.from(Array(Math.floor(Math.random() * 6 + 3)).keys()).map(
             (i, index) => (
               <img
                 key={index}
                 src="https://picsum.photos/50"
                 alt="member"
-                className="task-info__members-avatar"
+                className="workspace-header__member-avatar"
               />
             )
           )}
         </div>
       </div>
 
-      <div className="workspace__settings-btn">
-        <MdOutlineSettings size={30} />
-        <p className="workspace__settings-text">Settings</p>
+      <div className="workspace-header__right">
+        <div className="workspace-header__btn">
+          <FaUserPlus size={24} />
+          <p>Invite</p>
+        </div>
+
+        <div className="workspace-header__btn">
+          <FaFilter size={24} />
+          <p>Filter</p>
+        </div>
+
+        <div className="workspace-header__btn">
+          <FaBars size={24} />
+          <p>Show Menu</p>
+        </div>
       </div>
     </div>
   );
