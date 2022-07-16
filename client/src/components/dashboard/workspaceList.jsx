0db19b1cc075e6c41e57d35879a4b3ee8960e000
Nhat Minh <nhatminhc14@gmail.com>
2022-07-16 18:59:20 +0700

    [OS - 39] Rebase

--- client/src/components/dashboard/workspaceList.jsx
+++ client/src/components/dashboard/workspaceList.jsx
@@ -1,23 +1,14 @@
 import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import { useAccount } from '../../hooks';
 import {
-  AiOutlineArrowLeft,
-  AiOutlineArrowRight,
-  AiOutlineClose,
+	AiOutlineArrowLeft,
+	AiOutlineArrowRight,
+	AiOutlineClose,
 } from 'react-icons/ai';
 import { BsArrowRight } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 import { IoTextOutline } from 'react-icons/io5';
-import {
-  MdOutlineAddReaction,
-  MdAttachFile,
-  MdPersonAdd,
-} from 'react-icons/md';
-import routes from '../../routes';
-import '../../styles/components/workspaceListView.scss';
-import { methods, URL_Requests } from '../../APIs';
 
 import {
 	MdOutlineAddReaction,
@@ -26,112 +17,125 @@ import {
 } from 'react-icons/md';
 import routes from '../../routes';
 import '../../styles/components/workspaceListView.scss';
+import { methods, URL_Requests } from '../../APIs';
 
 function WorkspaceItemCard(props) {
-  const { itemId: id, title, description, members } = props;
-  const randomColor = ['#EEF7FB', '#f4f4f4', '#F8F1FF', '#FEF7EF'];
-  const navigate = useNavigate();
+	const { itemId: ownerId, title, description, members } = props;
+	const randomColor = ['#EEF7FB', '#f4f4f4', '#F8F1FF', '#FEF7EF'];
+	const navigate = useNavigate();
 
 	function handleClickWorkSpace() {
-		navigate(`${routes.workspaces.path}/${id}`);
+		navigate(`${routes.workspaces.path}/${ownerId}`);
 	}
 
-  return (
-    <div
-      className='workspace-item-card'
-      onClick={handleClickWorkSpace}
-      tabIndex={0}
-      style={{
-        backgroundColor:
-          randomColor[Math.floor(Math.random() * randomColor.length)],
-      }}
-    >
-      <p className='workspace-item-card-title'>{title}</p>
-      <p className='workspace-item-card-description'>{description}</p>
+	return (
+		<div
+			className='workspace-item-card'
+			onClick={handleClickWorkSpace}
+			tabIndex={0}
+			style={{
+				backgroundColor:
+					randomColor[Math.floor(Math.random() * randomColor.length)],
+			}}
+		>
+			<p className='workspace-item-card-title'>{title}</p>
+			<p className='workspace-item-card-description'>{description}</p>
 
-      <div className='workspace-item-card-footer'>
-        <div className='workspace-item-card-members'>
-          {_.isArray(members)
-            ? members.slice(0, 6).map((member, index) => {
-                return (
-                  <div
-                    className='workspace-item-card-member'
-                    style={{ left: 20 * index + 'px' }}
-                    key={index}
-                  >
-                    {index <= 4 ? (
-                      <img
-                        src={member}
-                        alt='avatar'
-                        style={{
-                          width: '30px',
-                          height: '30px',
-                        }}
-                      />
-                    ) : (
-                      <div>{`+${members.length - index}`}</div>
-                    )}
-                  </div>
-                );
-              })
-            : 'members'}
-        </div>
-        <BsArrowRight size={20} className='workspace-item-card-arrow' />
-      </div>
-    </div>
-  );
+			<div className='workspace-item-card-footer'>
+				<div className='workspace-item-card-members'>
+					{_.isArray(members)
+						? members.slice(0, 6).map((member, index) => {
+								return (
+									<div
+										className='workspace-item-card-member'
+										style={{ left: 20 * index + 'px' }}
+										key={index}
+									>
+										{index <= 4 ? (
+											<img
+												src={member}
+												alt='avatar'
+												style={{
+													width: '30px',
+													height: '30px',
+												}}
+											/>
+										) : (
+											<div>{`+${members.length - index}`}</div>
+										)}
+									</div>
+								);
+						  })
+						: 'members'}
+				</div>
+				<BsArrowRight size={20} className='workspace-item-card-arrow' />
+			</div>
+		</div>
+	);
 }
 
 function LeftArrow() {
 	const { isFirstItemVisible, scrollPrev } =
 		React.useContext(VisibilityContext);
 
-  return (
-    <button
-      disabled={isFirstItemVisible}
-      className='workspace-list-view-arrow'
-      onClick={() => scrollPrev()}
-    >
-      <AiOutlineArrowLeft size={20} />
-    </button>
-  );
+	return (
+		<button
+			disabled={isFirstItemVisible}
+			className='workspace-list-view-arrow'
+			onClick={() => scrollPrev()}
+		>
+			<AiOutlineArrowLeft size={20} />
+		</button>
+	);
 }
 
 function RightArrow() {
 	const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
 
-  return (
-    <button
-      disabled={isLastItemVisible}
-      className='workspace-list-view-arrow'
-      onClick={() => scrollNext()}
-    >
-      <AiOutlineArrowRight size={20} />
-    </button>
-  );
+	return (
+		<button
+			disabled={isLastItemVisible}
+			className='workspace-list-view-arrow'
+			onClick={() => scrollNext()}
+		>
+			<AiOutlineArrowRight size={20} />
+		</button>
+	);
 }
 
 export function WorkspaceListView() {
-  const [workspaces, setWorkspaces] = useState([]);
-  const { account } = useAccount();
-  const [showCreateWorkspace, setShowCreateWorkspace] = useState(false);
-  const handleCreateWorkspace = e => {
-    e.preventDefault();
-    setShowCreateWorkspace(false);
-  };
+	const [workspaces, setWorkspaces] = useState([]);
+	const [showCreateWorkspace, setShowCreateWorkspace] = useState(false);
 
-  const fetchWorkspaceList = async () => {
-    const response = await methods.get(
-      URL_Requests.users.workspaces(account.id),
-    );
-    console.log(response.data);
-    setWorkspaces(response.data);
-    return response.data;
-  };
+	function addNewWorkspace(workspace) {
+		try {
+			const response = methods.post(URL_Requests.createWorkspace, workspace);
+			setWorkspaces([
+				...workspaces,
+				{
+					title: workspace.title,
+					description: workspace.description,
+					ownerId: response.ownerId,
+					members: workspace.members,
+				},
+			]);
+			setShowCreateWorkspace(false);
+		} catch (error) {
+			console.log('có lỗi');
+			console.log(error);
+		}
+	}
 
-  useEffect(() => {
-    fetchWorkspaceList();
-  }, []);
+	useEffect(() => {
+		const workspaces = require('../../data/workspaces.json');
+		setWorkspaces(workspaces);
+	}, []);
+
+	return (
+		<React.StrictMode>
+			<div className='workspaceListView'>
+				<p className='workspaceList__title'>Workspaces</p>
+
 				<div className='workspaceList__container'>
 					<ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
 						{workspaces.map((workspace) => (
@@ -165,16 +169,16 @@ export function WorkspaceListView() {
 			console.error(' AddNewWorkspace is Null');
 		}
 
-		const [workspaceTitle, setWorkspaceTitle] = useState('');
-		const [workspaceDescription, setWorkspaceDescription] = useState('');
+		const [title, setTitle] = useState('');
+		const [description, setDescription] = useState('');
 
 		function handleOnCreateWorkspace(e) {
 			e.preventDefault();
 			// TODO: validate input
 			addNewWorkspace({
-				id: '100',
-				title: workspaceTitle,
-				description: workspaceDescription,
+				ownerId: '100',
+				title: title,
+				description: description,
 				members: [],
 			});
 		}
@@ -191,13 +195,13 @@ export function WorkspaceListView() {
 					type='text'
 					placeholder='Workspace name'
 					className='workspace-item__add-new-workspace-form-name'
-					onChange={(e) => setWorkspaceTitle(e.target.value)}
+					onChange={(e) => setTitle(e.target.value)}
 				/>
 				<textarea
 					type='text'
 					placeholder='Workspace description'
 					className='workspace-item__add-new-workspace-form-description'
-					onChange={(e) => setWorkspaceDescription(e.target.value)}
+					onChange={(e) => setDescription(e.target.value)}
 				/>
 				<div className='ulities-container'>
 					<div className='left'>
