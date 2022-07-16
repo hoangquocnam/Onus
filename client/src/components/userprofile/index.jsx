a1701bbdf7a6199d9f3143878d3150627445cefc
hqnam <81608889+hoangquocnam@users.noreply.github.com>
2022-07-16 17:43:55 +0700

    [OS-23] Create a new API + fetch real data (#29)

--- client/src/components/userprofile/index.jsx
+++ client/src/components/userprofile/index.jsx
@@ -1,27 +1,25 @@
-import "../../styles/pages/userprofile.css";
-import {useAccount} from "../../hooks";
-import {CgPathCrop, CgProfile} from "react-icons/cg";
-import {TbSettings} from "react-icons/tb";
-import {useState} from "react";
-import { toast } from 'react-toastify'
+import '../../styles/pages/userprofile.css';
+import { useAccount } from '../../hooks';
+import { CgProfile } from 'react-icons/cg';
+import { TbSettings } from 'react-icons/tb';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 function UserProfile() {
-
   const [isEditing, setEditing] = useState(false);
 
   const { account, updateProfile } = useAccount();
 
   const genderOptions = [
-    { label: "Male", value: "male" },
-    { label: "Female", value: "female" },
+    { label: 'Male', value: 'male' },
+    { label: 'Female', value: 'female' },
   ];
 
-  
   const [data, setData] = useState({
     fullname: account.fullname,
     gender: account.gender,
     username: account.username,
-    email: account.email
+    email: account.email,
   });
 
   function handleInputChange(e) {
@@ -32,78 +30,82 @@ function UserProfile() {
   }
 
   function handleUpdateProfile() {
-    if(isEditing) {
+    if (isEditing) {
       toast.promise(updateProfile(data), {
-      pending: "Loading...",
-      success: {
-        render() {
-          return "Update profile successfully";
+        pending: 'Loading...',
+        success: {
+          render() {
+            return 'Update profile successfully';
+          },
+          autoClose: 1000,
         },
-        autoClose: 1000,
-      },
-      error: "Update profile failed",
-    })}
+        error: 'Update profile failed',
+      });
+    }
 
     setEditing(!isEditing);
   }
 
   return (
-    <div className="user-profile__page">
-      <div className="user-profile__settings">
-        <h2 className="user-profile__title">Settings</h2>
+    <div className='user-profile__page'>
+      <div className='user-profile__settings'>
+        <h2 className='user-profile__title'>Settings</h2>
       </div>
-      <div className="user-profile__container">
-        <nav className="profile-nav__container">
-          <ul className="profile-nav__list">
-            <li className="profile-nav__item profile-nav__item--active">
-              <CgProfile className="public-profile__icon"/>
-              <a href="#">Public profile</a>
+      <div className='user-profile__container'>
+        <nav className='profile-nav__container'>
+          <ul className='profile-nav__list'>
+            <li className='profile-nav__item profile-nav__item--active'>
+              <CgProfile className='public-profile__icon' />
+              <a href='#'>Public profile</a>
             </li>
-            <li className="profile-nav__item">
-              <TbSettings className="public-profile__icon"/>
-              <a href="#">Account Settings</a>
+            <li className='profile-nav__item'>
+              <TbSettings className='public-profile__icon' />
+              <a href='#'>Account Settings</a>
             </li>
           </ul>
         </nav>
-        <div className="public-profile__container">
-          <h2 className="public-profile__title">Public profile</h2>
-          <div className="public-profile__picture">
+        <div className='public-profile__container'>
+          <h2 className='public-profile__title'>Public profile</h2>
+          <div className='public-profile__picture'>
             <img
-              src="https://api.minimalavatars.com/avatar/random/png"
-              alt="Avatar"
-              className="public-profile__image"
+              src='https://api.minimalavatars.com/avatar/random/png'
+              alt='Avatar'
+              className='public-profile__image'
             />
-            <div className="public-profile__btn">
-              <button className="public-profile__changeBtn">
+            <div className='public-profile__btn'>
+              <button className='public-profile__changeBtn'>
                 Change picture
               </button>
-              <button className="public-profile__editBtn" onClick={handleUpdateProfile}>
-                {isEditing? 'Confirm' : 'Edit profile' }
+              <button
+                className='public-profile__editBtn'
+                onClick={handleUpdateProfile}
+              >
+                {isEditing ? 'Confirm' : 'Edit profile'}
               </button>
             </div>
-          </div>  
-          <form className="public-profile__form">
-            <div className="public-profile__info">
-              <div className="public-profile__fullname">
+          </div>
+          <form className='public-profile__form'>
+            <div className='public-profile__info'>
+              <div className='public-profile__fullname'>
                 <label>Full name: </label>
                 <input
-                  type="text"
-                  name="fullname"
-                  onChange = {handleInputChange}
-                  value= {data.fullname}
-                  placeholder="Full name"
-                  disabled = {!isEditing}
+                  type='text'
+                  name='fullname'
+                  onChange={handleInputChange}
+                  value={data.fullname}
+                  placeholder='Full name'
+                  disabled={!isEditing}
                 />
               </div>
-              <div className="public-profile__gender">
+              <div className='public-profile__gender'>
                 <label>Gender: </label>
                 <select
-                  name="gender"
-                  disabled = {!isEditing}
+                  name='gender'
+                  disabled={!isEditing}
                   onChange={handleInputChange}
                   defaultValue={data.gender}
                 >
-                  {genderOptions.map((item) => (
+                  {genderOptions.map(item => (
                     <option
                       key={item.value}
                       value={item.value}
@@ -115,26 +117,26 @@ function UserProfile() {
                 </select>
               </div>
             </div>
-            <div className="public-profile__username">
+            <div className='public-profile__username'>
               <label>Username: </label>
               <input
-                type="text"
-                name="username"
+                type='text'
+                name='username'
                 onChange={handleInputChange}
-                value= {data.username}
-                placeholder="Username"
-                disabled = {!isEditing}
+                value={data.username}
+                placeholder='Username'
+                disabled={!isEditing}
               />
             </div>
-            <div className="public-profile__email">
+            <div className='public-profile__email'>
               <label>Email: </label>
               <input
-                type="email"
-                name="email"
+                type='email'
+                name='email'
                 onChange={handleInputChange}
-                value= {data.email}
-                placeholder="Email"
-                disabled = {!isEditing}
+                value={data.email}
+                placeholder='Email'
+                disabled={!isEditing}
               />
             </div>
           </form>
